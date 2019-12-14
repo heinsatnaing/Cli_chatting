359f7399772af28c472a61cd9d2dc6f41d343b01
Hein Sat Naing <50688597+heinsatnaing@users.noreply.github.com>
2019-12-14 22:49:21 +0700

    Update chat_server.sh

--- chat_server.sh
+++ chat_server.sh
@@ -4,7 +4,7 @@
 
 #Main program
 echo "Chatting program started!"
-#yum install netcat -y 2>/dev/null 1>/dev/null
+yum install netcat -y 2>/dev/null 1>/dev/null
 read -p "Which port do you want to open? :>>" open_port
 read -p "Add password :>>" passwd
 passwd2=$(echo "$passwd" | openssl dgst -md5 | awk '{print $2}')
